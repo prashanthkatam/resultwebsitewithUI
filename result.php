2b718d05debfcc860100622efc8f5f7183d33972
root <root@ip-172-31-37-155.us-west-2.compute.internal>
2022-12-06 10:40:51 +0000

    Added Table Format View For The Results

--- result.php
+++ result.php
@@ -20,6 +20,13 @@
 <html lang="en">
 
   <head>
+               <style>
+    table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+      background-color: pink;
+    }
+    </style>
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
